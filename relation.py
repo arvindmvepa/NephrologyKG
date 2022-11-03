a391ce3a86a85e0484c76ce4285ec2bd849b5960
Arvind Vepa <arvind.m.vepa@gmail.com>
2022-11-02 19:49:07 -0700

    progress

--- relation.py
+++ relation.py
@@ -4,9 +4,28 @@ import os
 import csv
 
 BASE_URL = "https://uts-ws.nlm.nih.gov/rest/content/current/CUI/"
-API_KEY = "1a313e43-cbac-4194-87d3-2b2b43e63eb9"  
+API_KEY = "1a313e43-cbac-4194-87d3-2b2b43e63eb9"
 
-def get_relations(CUI, info = "partial", write_to_file = False):
+
+async def async_get_relations(CUI, session):
+    # get all relations related to the entity with given CUI
+    api_url = "https://uts-ws.nlm.nih.gov/rest/content/current/CUI/" + CUI + "/relations?apiKey=" + API_KEY + "&sabs=MTH"
+    retry=False
+    while True:
+        try:
+            async with session.get(url=api_url) as response:
+                resp = await response.read()
+                resp = resp.decode("utf-8")
+                resp_json = json.loads(resp)
+                if retry:
+                    print("Retry successful")
+                return resp_json
+        except Exception as e:
+            print(f"Error retrieving {api_url} due to {e}. Retrying")
+            retry = True
+
+
+def get_relations(CUI, info = "partial"):
     # get all relations related to the entity with given CUI
 
     if (info == "partial"):
@@ -16,19 +35,13 @@ def get_relations(CUI, info = "partial", write_to_file = False):
 
     http = urllib3.PoolManager()
     response = http.request('GET', api_url)
+    if not response.data:
+        return {}
     json_response = json.loads(response.data)
-    
-    if (write_to_file):
-        # Serializing json 
-        json_object = json.dumps(json_response, indent=4)
-  
-        # Writing to sample.json
-        with open("relations_" + CUI + ".json", "w") as outfile:
-            outfile.write(json_object)
 
     return json_response
 
-def get_entity(CUI, write_to_file = False):
+def get_entity(CUI):
     # get information about an entity using CUI
 
     api_url = "https://uts-ws.nlm.nih.gov/rest/content/current/CUI/" + CUI + "?apiKey=" + API_KEY
@@ -36,13 +49,6 @@ def get_entity(CUI, write_to_file = False):
     http = urllib3.PoolManager()
     response = http.request('GET', api_url)
     json_response = json.loads(response.data)
-
-    if (write_to_file):
-        json_object = json.dumps(json_response, indent=4)
-  
-        # Writing to sample.json
-        with open("relations_" + CUI + ".json", "w") as outfile:
-            outfile.write(json_object)
     
     return json_response
 
@@ -85,17 +91,17 @@ def get_all_relations(CUI_list, info = "partial"):
                         for j in range(len(related_list)):
                             if (CUI_list[i] == related_list[j]):
                                 info_for_save = {
-                                    "head" : CUI_list[index], 
-                                    "tail" : related_list[j], 
-                                    # "ui" : rel["ui"], 
-                                    "relationLabel" : rel["relationLabel"], 
-                                    "additionalRelationLabel" : rel["additionalRelationLabel"], 
+                                    "head" : CUI_list[index],
+                                    "tail" : related_list[j],
+                                    # "ui" : rel["ui"],
+                                    "relationLabel" : rel["relationLabel"],
+                                    "additionalRelationLabel" : rel["additionalRelationLabel"],
                                     "source" : 0
                                 }
                                 json_object = json.dumps(info_for_save)
                                 with open("all_rel.jsonl", "a") as outfile:
                                     print(json_object, file = outfile)
-                
+
 
                     # all combinations
                     '''''
@@ -128,6 +134,8 @@ get_all_relations(test_CUI3)
 # real applications
 
 # change to where CUI_lists.csv is stored
+
+"""
 os.chdir("/home/felix/pyfiles")
 with open('CUI_lists.csv', 'r') as read_obj:
     csv_reader = csv.reader(read_obj)
@@ -140,4 +148,5 @@ for list in CUI_list:
     i = i + 1
     get_all_relations(list)
     print(i, "out of", list_len, "finished")
+"""
     
---
+++ subgraph.py
@@ -0,0 +1,79 @@
+import json
+import asyncio
+import aiohttp
+from relation import async_get_relations
+
+
+def get_concepts_from_questions(linked_question_file):
+    # load linked question file
+    with open(linked_question_file) as f:
+        lines = f.readlines()
+    json_lines = [json.loads(line) for line in lines]
+
+    # get all the concepts per question text
+    question_cui_name_pairs = []
+    for json_line in json_lines:
+        ent_results = [v for ent in json_line['question']['stem_ents']
+                       for k, v in ent.items() if k == 'linking_results']
+        cui_name_pairs = [(ent['Concept ID'], ent['Canonical Name']) for ent_matches in ent_results
+                for ent in ent_matches]
+        question_cui_name_pairs.append(cui_name_pairs)
+
+    # get all the concepts per choice per question
+    answer_cui_name_pairs = []
+    for json_line in json_lines:
+        choice_cuis = []
+        for choice in json_line['question']['choices']:
+            cuis = [(ent_match['Concept ID'], ent_match['Canonical Name'])
+                    for ent_results in choice['text_ents']
+                    for ent_match in ent_results['linking_results']]
+            choice_cuis.append(cuis)
+        answer_cui_name_pairs.append(choice_cuis)
+
+    return question_cui_name_pairs, answer_cui_name_pairs
+
+
+async def get_all_related_cuis_names(cui, session):
+    json_response = await async_get_relations(cui, session)
+    all_related_cuis_names = []
+    if "result" in json_response:
+        for rel in json_response["result"]:
+            url_split = rel["relatedId"].split('/')
+            related_cuis = [url_split[-1]]
+            related_names = [rel["relatedIdName"]]
+            if len(related_cuis) > 0:
+                all_related_cuis_names.extend([(rel_cui, rel_name, rel["relationLabel"], rel["additionalRelationLabel"])
+                                         for rel_cui, rel_name in zip(related_cuis, related_names)])
+    return all_related_cuis_names
+
+
+async def get_two_hop_paths(source_cui_name_pairs, dest_cui_name_pairs, session, index):
+    two_hop_paths = []
+    dest_cuis = [cui for (cui, name_pair) in dest_cui_name_pairs]
+    for i, (source_cui,source_name) in enumerate(source_cui_name_pairs):
+        print(f"{index}  {i}")
+        int_cuis_names_rels = await get_all_related_cuis_names(source_cui, session)
+        for j, (int_cui, int_name, int_rel1, int_rel2) in enumerate(int_cuis_names_rels):
+            print(f"{index}  {i}.{j}")
+            target_cuis_names_rels = await get_all_related_cuis_names(int_cui, session)
+            two_hop_paths.extend([[(source_cui, source_name, int_cui, int_name, int_rel1, int_rel2),
+                                   (int_cui, int_name, target_cui, target_name, target_rel1, target_rel2)]
+                                  for target_cui, target_name, target_rel1, target_rel2 in target_cuis_names_rels
+                                  if target_cui in dest_cuis])
+    return two_hop_paths
+
+
+async def get_twohop_subgraph(linked_question_file):
+    """Get 2-hop subgraphs from question_cuis and answer_cuis using parallelized approach.
+    For parallelizing http requests, reference this:
+    https://stackoverflow.com/questions/57126286/fastest-parallel-requests-in-python
+    """
+    #TODO: Fix bug on how question/answers are being extracted. Currently looking at all two hop between all questions and all choices which is inefficient
+    question_cui_name_pairs, answer_cui_name_pairs = get_concepts_from_questions(linked_question_file)
+    question_cui_name_pairs, answer_cui_name_pairs = question_cui_name_pairs[:5], answer_cui_name_pairs[:5]
+    async with aiohttp.ClientSession() as session:
+        subgraphs = await asyncio.gather(*[get_two_hop_paths(q_cui_cui_name_pair, a_choice_cui_name_pair, session, (i,j))
+                                     for i, (q_cui_cui_name_pair, a_choices_cui_name_pairs) in enumerate(zip(question_cui_name_pairs, answer_cui_name_pairs))
+                                     for j, a_choice_cui_name_pair in enumerate(a_choices_cui_name_pairs)])
+    print("Finalized all. Return is a list of len {} outputs.".format(len(subgraphs)))
+    return subgraphs
