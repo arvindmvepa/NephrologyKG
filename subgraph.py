313bf34b9c363071cc7ddde339dcd8b020cca259
Arvind Vepa <arvind.m.vepa@gmail.com>
2022-12-07 00:51:59 -0800

    bug

--- subgraph.py
+++ subgraph.py
@@ -231,7 +231,7 @@ def get_two_hop_paths_from_db(source_cui_name_pairs, dest_cui_name_pairs, cursor
     for i, (source_cui,source_name) in enumerate(source_cui_name_pairs):
         for j, (dest_cui, dest_name) in enumerate(dest_cui_name_pairs):
             query = f"SELECT t1.CUI2,f1.STR,t1.REL,t1.RELA,t2.CUI1,f2.STR,t2.REL,t2.RELA FROM MRREL t1, MRREL t2, " \
-                    f"MRCONSO f1, MRCONSO f2 WHERE t1.CUI1='{source_cui}' and t1.CUI2=t2.CUI1 and t2.CUI2='{dest_cui}' " \ 
+                    f"MRCONSO f1, MRCONSO f2 WHERE t1.CUI1='{source_cui}' and t1.CUI2=t2.CUI1 and t2.CUI2='{dest_cui}' " \
                     "and t1.CUI2=f1.CUI and t2.CUI1=f2.CUI;"
             cursor.execute(query)
             for res in cursor:
