f2c1d9a28aed6c08705bad4ad8c911cd827065a2
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-27 19:39:26 -0800

    example fine tuning script

--- example_fine_tune.py
+++ example_fine_tune.py
@@ -26,7 +26,6 @@ from transformers import (
 )
 
 
-
 def dataset_preprocess():
     # download dataset
     dataset = load_dataset('web_nlg', 'release_v3.0_en')
@@ -60,7 +59,25 @@ def dataset_preprocess():
     dataset_df.to_csv('Web_NLG.csv', index=False)
 
 
-def load_llm():
+def load_dataset():
+    data = load_dataset("csv", data_files="Web_NLG.csv")
+    def generate_prompt(data_point):
+        return f"""
+    INPUT: {data_point["text"]}
+    OUTPUT: {data_point["triple"]}
+    """.strip()
+
+    def generate_and_tokenize_prompt(data_point):
+        full_prompt = generate_prompt(data_point)
+        tokenized_full_prompt = tokenizer(full_prompt, padding=True, truncation=True)
+        return tokenized_full_prompt
+
+    data = data["train"].shuffle().map(generate_and_tokenize_prompt)
+    print(data)
+    return data
+
+
+def load_llm_from_huggingface():
     MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
     TOKENIZER_NAME = "HuggingFaceH4/zephyr-7b-beta"
 
@@ -97,7 +114,54 @@ def load_llm():
     model = get_peft_model(model, config)
     return model, tokenizer
 
-def test_text_gen(model, tokenizer):
+
+def train_llm(model, data):
+    OUTPUT_DIR = "experiments"
+    training_args = transformers.TrainingArguments(
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=4,
+        num_train_epochs=150,
+        learning_rate=2e-4,
+        fp16=True,
+        save_total_limit=3,
+        logging_steps=1,
+        output_dir=OUTPUT_DIR,
+        max_steps=150,
+        optim="paged_adamw_8bit",
+        lr_scheduler_type="cosine",
+        warmup_ratio=0.05,
+        report_to="tensorboard",
+    )
+
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=data,
+        args=training_args,
+        data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+    )
+    model.config.use_cache = False
+    trainer.train()
+    model.save_pretrained("trained-model")
+
+
+def load_llm():
+    PEFT_MODEL = "trained-model"
+
+    config = PeftConfig.from_pretrained(PEFT_MODEL)
+    model = AutoModelForCausalLM.from_pretrained(
+        config.base_model_name_or_path,
+        return_dict=True,
+        # quantization_config=bnb_config,
+        device_map="cuda:0",
+        # trust_remote_code=True,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    model = PeftModel.from_pretrained(model, PEFT_MODEL)
+    return model, tokenizer
+
+def test1_text_gen(model, tokenizer, device="cuda:0"):
     prompt = f"""
     how can i clean here?
     """.strip()
@@ -111,8 +175,6 @@ def test_text_gen(model, tokenizer):
     generation_config.pad_token_id = tokenizer.eos_token_id
     generation_config.eos_token_id = tokenizer.eos_token_id
 
-    device = "cuda:0"
-
     encoding = tokenizer(prompt, return_tensors="pt").to(device)
     with torch.inference_mode():
         outputs = model.generate(
@@ -123,6 +185,49 @@ def test_text_gen(model, tokenizer):
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
 
+def test2_text_gen(model, tokenizer, device="cuda:0"):
+    generation_config = model.generation_config
+    generation_config.max_new_tokens = 256
+    generation_config.temperature = 0.4
+    generation_config.top_p = 0.2
+    generation_config.num_return_sequences = 1
+    generation_config.pad_token_id = tokenizer.eos_token_id
+    generation_config.eos_token_id = tokenizer.eos_token_id
+
+    def generate_response(question: str) -> str:
+        prompt = f"""
+    [S]
+    INPUT: {question}
+    OUTPUT: 
+    """.strip()
+        encoding = tokenizer(prompt, return_tensors="pt").to(device)
+        with torch.inference_mode():
+            outputs = model.generate(
+                input_ids=encoding.input_ids,
+                attention_mask=encoding.attention_mask,
+                generation_config=generation_config,
+            )
+        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        assistant_start = "OUTPUT:"
+        response_start = response.find(assistant_start)
+        return response[response_start + len(assistant_start):].strip()
+
+    prompt = """ 
+    Bangkok It is the capital and most populous city of Thailand. It is the center of government, education, transportation.
+    Finance, banking, commerce, communications and national prosperity. Located on the Chao Phraya River Delta. 
+    The Chao Phraya River flows through it and divides the city into 2 sides: the Phra Nakhon side and the Thonburi side. 
+    Bangkok has a total area of 1,568.737 sq. km. with a registered population of more than 6 million people, making Bangkok classified as a Primate City. 
+    It is said that Bangkok used to be "The greatest city in the world" because in the year 2000
+     """
+    print(generate_response(prompt))
+
 if __name__ == '__main__':
+    device = "cuda:0"
+    model, tokenizer = load_llm_from_huggingface()
+    test1_text_gen(model, tokenizer, device=device)
+    dataset_preprocess()
+    data = load_dataset()
+    train_llm(model, data)
     model, tokenizer = load_llm()
-    test_text_gen(model, tokenizer)
+    test2_text_gen(model, tokenizer, device=device)
