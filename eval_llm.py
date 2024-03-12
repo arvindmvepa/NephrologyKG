246d264a37a2e5ed3c1facd7f8e5e75d61fe0f87
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-03-12 16:47:25 -0700

    add decoding strat

--- eval_llm.py
+++ eval_llm.py
@@ -5,7 +5,6 @@ from transformers import (
     BitsAndBytesConfig,
     TrainingArguments,
     Trainer,
-    pipeline
 )
 from peft import (
     PeftConfig,
@@ -13,11 +12,13 @@ from peft import (
 )
 import csv
 
-pipeline_task_keys = {'text-generation': 'generated_text'}
+decoding_strats= {"greedy": {"num_beams": 1, "do_sample": False},
+                  "contrast": {"penalty_alpha": 0.6, "top_k": 4},
+                  "beam": {"num_beams": 4, "do_sample": False},
+                  "top_k": {"do_sample": True, "top_k": 50}}
 
 
-def eval_llm(model_name, save_file, questions=[], prompt="", pipeline_task='text-generation', max_new_tokens=1000,
-             used_lora=True):
+def eval_llm(model_name, save_file, questions=[], prompt="", max_new_tokens=1000, decoding_strat="greedy", used_lora=True):
     if used_lora:
         config = PeftConfig.from_pretrained(model_name)
         model = AutoModelForCausalLM.from_pretrained(
@@ -31,11 +32,14 @@ def eval_llm(model_name, save_file, questions=[], prompt="", pipeline_task='text
         model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         tokenizer.pad_token = tokenizer.eos_token
-    generator = pipeline(pipeline_task, model=model, tokenizer=tokenizer, max_new_tokens=max_new_tokens, device_map="auto")
+    decoding_kwargs = decoding_strats[decoding_strat]
     content = []
     for question in questions:
         question = prompt + question
-        answer = generator(question)[0][pipeline_task_keys[pipeline_task]]
+        inputs = tokenizer(question, return_tensors="pt").to("cuda")
+        generated_ids = model.generate(**inputs, num_return_sequences=1, max_new_tokens=max_new_tokens, **decoding_kwargs)
+        answer = tokenizer.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
+        answer = answer.split(question)[-1]
         content.append((question, answer))
         print(f"question: {question}")
         print(f"answer: {answer}")
@@ -55,9 +59,9 @@ if __name__ == '__main__':
     per_device_train_batch_size=8
     save_eval_steps=2000
     data_path = "neph.csv"
-    pipeline_task = "text-generation"
     model_name = f"neph_blocksize{block_size}_optm{optimizer}_fp16{fp16}_bs{per_device_train_batch_size}"
-    tag = "_v2"
+    decoding_strat = "beam"
+    tag = "_v3"
     prompt= "Extract all the entities from the ensuing paragraph. Please provide them in a list format: "
     questions = [# q1
                  "Glomerular hypertrophy may be marker of FSGS. Glomerular enlarge-\n"
@@ -82,8 +86,8 @@ if __name__ == '__main__':
                  "public.This term includes the continuum of kidney dysfunction from mild kidney damage to\n"
                  "kidney failure, and it also includes the term, end-stage renal disease (ESRD)."
                  ]
-    eval_llm(model_name, model_name.replace('/','_') + f"_{pipeline_task}_entities" + tag + ".csv", questions=questions,
-             prompt=prompt)
+    eval_llm(model_name, model_name.replace('/','_') + f"_{decoding_strat}_entities" + tag + ".csv",
+             decoding_strat=decoding_strat, questions=questions, prompt=prompt)
 
 
 
