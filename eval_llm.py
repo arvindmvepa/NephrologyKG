01a34a2b182338ca003081608a0a39e3ebf4ec0c
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-03-12 19:52:24 -0700

    debug stuff

--- eval_llm.py
+++ eval_llm.py
@@ -41,9 +41,10 @@ def eval_llm(model_name, save_file, questions=[], prompt="", max_new_tokens=1000
     for question in questions:
         question = prompt + question
         inputs = tokenizer(question, return_tensors="pt").to("cuda")
-        generated_ids = model.generate(**inputs, num_return_sequences=1, max_new_tokens=max_new_tokens, **decoding_kwargs)
+        generated_ids = model.generate(**inputs, num_return_sequences=1, max_new_tokens=max_new_tokens, **decoding_kwargs)[0]
         answer = tokenizer.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        #answer = answer.split(question)[-1]
+        answer = "".join(answer)
+        answer = answer.split(question)[-1]
         if debug:
             content.append((question, answer, generated_ids))
         else:
