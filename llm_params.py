2484bfbac4dbc6bc51976279f974cdc3ee1ba983
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-03-19 16:42:05 -0700

    bug

--- llm_params.py
+++ llm_params.py
@@ -1,2 +1,14 @@
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    Trainer,
+    LlamaForCausalLM,
+    LlamaTokenizer
+)
+
+
 llm_dict = {"llama": {"model": LlamaForCausalLM, "tokenizer": LlamaTokenizer},
             "default": {"model": AutoModelForCausalLM, "tokenizer": AutoTokenizer}}
